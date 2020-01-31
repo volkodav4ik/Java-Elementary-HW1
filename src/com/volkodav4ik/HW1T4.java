433c959168b98545add5adb95ef33b7a560c719d
Artem Vovk <volkodav4ik1990@gmail.com>
2020-01-31 13:28:08 +0200

    Add Task 4

---
+++ src/com/volkodav4ik/HW1T4.java
@@ -0,0 +1,23 @@
+package com.volkodav4ik;
+/*
+4) Написать метод который вернет String в которой будут в одну строку
+числа от 1 до 30. Каждое число при этом взять в круглые скобки ()
+*/
+public class HW1T4 {
+
+    private static final int MAX_VALUE = 30;
+
+    public static void main(String[] args) {
+        String s = "";
+        System.out.println(createAndInitString(s));
+    }
+
+    private static String createAndInitString(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 1; i <= MAX_VALUE; i++) {
+            sb.append("(" + i + ")");
+        }
+        s = sb.toString();
+        return s;
+    }
+}
